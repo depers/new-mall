f0fa0c4a1d5941642831e7cce9df2e82f1000d9d
depers <dev_fengxiao@163.com>
2023-12-29 07:40:50 +0800

    单体项目初始化

---
+++ mall-api/src/main/java/cn/bravedawn/api/Application.java
@@ -0,0 +1,18 @@
+package cn.bravedawn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author : fengx9
+ * @Project : new-mall
+ * @Date : Created in 2023-12-28 10:58
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
