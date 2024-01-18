18c5c457ab5fbf9ed367f2e11522ec2284714803
depers <dev_fengxiao@163.com>
2024-01-18 23:21:16 +0800

    2024-01-18

---
+++ doc/第二阶段/1.LVS+Nginx实现高可用集群/keepalive+naginx-双机主备/备用节点配置文件-192.168.156.135/check_nginx_alive_or_dead.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+A=`ps -C nginx --no-header |wc -l`
+# 判断nginx是否宕机
+if [ $A -eq 0 ];then
+    # 这里写自己安装的nginx的目录
+    /usr/local/myapp/program/nginx/sbin/nginx
+    # 等待3秒再次检查nginx，如果没有启动成功，则停止keepalived，使其启动备用机
+    sleep 3
+    if [ `ps -C nginx --no-header |wc -l` -eq 0 ];then
+        killall keepalived
+    fi
+fi
