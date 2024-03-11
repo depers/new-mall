3a6ff1622193f05d8461fa398f53300f01c7fc74
depers <dev_fengxiao@163.com>
2024-03-11 22:51:10 +0800

    2024-03-11

--- distribute/lock-demo/src/main/java/cn/bravedawn/demo/DistributeDemoApplication.java
+++ distribute/lock-demo/src/main/java/cn/bravedawn/demo/DistributeDemoApplication.java
@@ -7,6 +7,7 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
